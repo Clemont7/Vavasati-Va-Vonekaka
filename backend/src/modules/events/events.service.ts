8c23cb420161008e79a420200430e5e974eda02d
Ludmila Emilia <95902126+ludyy908@users.noreply.github.com>
2026-08-19 20:57:11 +0200

    Added some pipes and dtos

--- backend/src/modules/events/events.service.ts
+++ backend/src/modules/events/events.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EventsService {
-  list() {
-    return [];
+  getList() {
+    return [ {
+        title: 'Lançamento da 3V',
+        date: new Date('2026-11-11'),
+      },];
+  }
+
+updateRsvp(id: String, event: String, date: Date){
+  return 'Rvsp com Sucesso!';
+  /*Save information of user and event in database*/
   }
 }
